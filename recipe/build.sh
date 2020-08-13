008917028e2e408e2357d431ca4709e35f23583c
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-08-13 16:17:57 +0200

    Attempt to fix mac os recipe.

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,3 +8,7 @@ make
 # The make script uses the $PREFIX env variable to install the application.
 # The 'gzip' application (which is not gzip) will be renamed libdeflate-gzip. 
 make install
+
+# Redundant for linux. Necessary for Mac OS X.
+cp libdeflate.* $PREFIX/lib/
+
