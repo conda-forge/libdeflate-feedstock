e81ecd4ae12c8253472bcd805b609b54d1ce8e9f
Mark Harfouche <mark.harfouche@gmail.com>
2020-12-25 14:27:03 -0500

    Ensure that the static libraries aren't installed

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -x
 
-export C_INCLUDE_PATH=${PREFIX}/include
-
 make
 
 # The make script uses the $PREFIX env variable to install the application.
 # The 'gzip' application (which is not gzip) will be renamed libdeflate-gzip.
 make install
 
-
-# Remove static libraries
-rm -f libdeflate.a
 # Redundant for linux. Necessary for Mac OS X.
 cp libdeflate.* $PREFIX/lib/
+# Remove static libraries that are installed automatically
+rm -f $PREFIX/lib/libdeflate.a
